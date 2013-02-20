2a763a3e820371906ec303908666809c46e458c9
91195604 <91195604@alunos.ipbeja.pt>
2013-02-20 16:48:52 +0000

    integra??o com interface gr?fica

--- src/t.py
+++ src/t.py
@@ -4,9 +4,11 @@
 from impacket.ImpactDecoder import EthDecoder
 from impacket.ImpactPacket import IP, TCP, UDP, ICMP
 from pcapy import *
+from interface_teste import MainMenu
+from struct import *
 import datetime
 import socket
-from struct import *
+import wx
 
 #classe de Colorização
 class bcolors:
@@ -29,119 +31,140 @@ class bcolors:
         pass
     pass
 
-
-decoder = EthDecoder()
-
-def callback(jdr, data):
-    packet = decoder.decode(data)
-    child = packet.child()
-    if isinstance(child, IP):
-        child = child.child()
-        if isinstance(child, TCP):
-            if child.get_th_dport() == 143 or child.get_th_dport() == 993:
-                print 'IMAP'
-                print dir(child)
-                print child.get_data_as_string()
-                print child.get_buffer_as_string()
-                print child.get_bytes()
-
-    pass
-
-
-def testeCap(resposta):
+class SniffImap(object):
     
-    if resposta == "1":
-        caminhoFile = raw_input("Introduza o caminho do ficheiro: (exemplo: /home/ficheiros/nomeFile.pcap): ")
-        return caminhoFile
-        pass
-    elif resposta == "2":
-        #list all devices
-        devices = findalldevs()
-        print devices
+    decoder = EthDecoder()
+    def __init__(self):
         
-        #ask user to enter device name to sniff
-        print "Available devices are :"
-        for d in devices :
-            print d
+        app = wx.PySimpleApp(0)
+        wx.InitAllImageHandlers()
+        self.frame_1 = MainMenu(None, -1, "")
+        app.SetTopWindow(self.frame_1)
+        self.frame_1.Show()
         
-        dev = raw_input("Enter device name to sniff : ")
+      
         
-        print "Sniffing device " + dev
-        return dev
-        pass
+        #Começo da captura
+        resposta = self.dialogoInicial()
+        while (resposta != "1" or resposta != "2"):
+            #estatistica de um ficheiro pcap
+            if resposta == "1":
+                caminhoFile = self.testeCap(resposta)
+                print bcolors.OKBLUE + "Estatisticas a efectuar no file: " + caminhoFile + "!" + bcolors.ENDC
+                pcap = open_offline(caminhoFile)
+                i = 1
+                pcap.setfilter("tcp port 143 or tcp port 993")
+                (header, packet) = pcap.next()
+                while header:
+                    #print ('%d -> %s: captured %d bytes, truncated to %d bytes'
+                    #%(i, datetime.datetime.now(), header.getlen(), header.getcaplen()))
+                         
+                    lista = self.analisePacote(packet)
+                    #print lista
+                    print i
+                    self.frame_1.field_lineOfList_ctrl(i, lista)
+                    i +=1
+                    (header, packet) = pcap.next()
+                break
+                pass
+            
+            #estatistica de uma captura
+            elif resposta == "2":
+                print bcolors.OKGREEN + "Vai ser efecutada uma Captura, mas siga as instruções seguintes!" + bcolors.ENDC
+                dev = self.testeCap(resposta)
+                pcap = open_live(dev , 65536 , 1 , 0)
+                pcap.loop(0, self.callback)
+                
+                pass
+            else:
+                print bcolors.FAIL + "A sua resposta não é válida, volte a responder!" + bcolors.ENDC
+                resposta = self.dialogoInicial()
+                pass
+            pass
+        app.MainLoop()
     
-    pass
+        
+   
 
-#IMAP Port 143
-#IMAP SSL Port 993
-#IMAP StartTLS Port 143
-
-def dialogoInicial():    
-    print "Pretende fazer uma estatistica de um ficheiro pcap ou fazer uma captura?"
-    print "Caso: ficheiro pcap  -> 1"
-    print "Caso: fazer captura  -> 2"
+    def callback(self, jdr, data):
+        packet = self.decoder.decode(data)
+        child = packet.child()
+        if isinstance(child, IP):
+            child = child.child()
+            if isinstance(child, TCP):
+                if child.get_th_dport() == 143 or child.get_th_dport() == 993:
+                    print 'IMAP'
+                    print dir(child)
+                    print child.get_data_as_string()
+                    print child.get_buffer_as_string()
+                    print child.get_bytes()
     
-    resposta = raw_input("Faça a sua escolha: ")
-    return resposta
-    pass
-
-
-#Convert a string of 6 characters of ethernet address into a dash separated hex string
-def eth_addr (a) :
-    b = "{0:02x}:{1:02x}:{2:02x}:{3:02x}:{4:02x}:{5:02x}".\
-        format(ord(a[0]) , ord(a[1]) , ord(a[2]), ord(a[3]), ord(a[4]) , ord(a[5]))
-    return b
-'''
-Analisar pacote
-'''
-def analisePacote(packet):
-    lista = []
-    #parse ethernet header
-    eth_length = 14
-    
-    eth_header = packet[:eth_length]
-    eth = unpack('!6s6sH' , eth_header)
-    eth_protocol = socket.ntohs(eth[2])
-    
-    #Destination MAC,  Source MAC, Protocolo
-    lista.append([eth_addr(packet[0:6]),eth_addr(packet[6:12]), str(eth_protocol)])
-    return lista
-    pass
-
-
-
-resposta = dialogoInicial()
-while (resposta != "1" or resposta != "2"):
-    #estatistica de um ficheiro pcap
-    if resposta == "1":
-        caminhoFile = testeCap(resposta)
-        print bcolors.OKBLUE + "Estatisticas a efectuar no file: " + caminhoFile + "!" + bcolors.ENDC
-        pcap = open_offline(caminhoFile)
-        i = 1
-        pcap.setfilter("tcp port 143 or tcp port 993")
-        (header, packet) = pcap.next()
-        while header:
-            #print ('%d -> %s: captured %d bytes, truncated to %d bytes'
-            #%(i, datetime.datetime.now(), header.getlen(), header.getcaplen()))
-                 
-            lista = analisePacote(packet)
-            print lista
-            i +=1
-            (header, packet) = pcap.next()
-        break
         pass
     
-    #estatistica de uma captura
-    elif resposta == "2":
-        print bcolors.OKGREEN + "Vai ser efecutada uma Captura, mas siga as instruções seguintes!" + bcolors.ENDC
-        dev = testeCap(resposta)
-        pcap = open_live(dev , 65536 , 1 , 0)
-        pcap.loop(0, callback)
+    
+    def testeCap(self, resposta):
+        
+        if resposta == "1":
+            caminhoFile = raw_input("Introduza o caminho do ficheiro: (exemplo: /home/ficheiros/nomeFile.pcap): ")
+            return caminhoFile
+            pass
+        elif resposta == "2":
+            #list all devices
+            devices = findalldevs()
+            print devices
+            
+            #ask user to enter device name to sniff
+            print "Available devices are :"
+            for d in devices :
+                print d
+            
+            dev = raw_input("Enter device name to sniff : ")
+            
+            print "Sniffing device " + dev
+            return dev
+            pass
         
         pass
-    else:
-        print bcolors.FAIL + "A sua resposta não é válida, volte a responder!" + bcolors.ENDC
-        resposta = dialogoInicial()
+    
+    #IMAP Port 143
+    #IMAP SSL Port 993
+    #IMAP StartTLS Port 143
+    
+    def dialogoInicial(self):    
+        print "Pretende fazer uma estatistica de um ficheiro pcap ou fazer uma captura?"
+        print "Caso: ficheiro pcap  -> 1"
+        print "Caso: fazer captura  -> 2"
+        
+        resposta = raw_input("Faça a sua escolha: ")
+        return resposta
         pass
-    pass
+    
+    
+    #Convert a string of 6 characters of ethernet address into a dash separated hex string
+    def eth_addr (self, a) :
+        b = "{0:02x}:{1:02x}:{2:02x}:{3:02x}:{4:02x}:{5:02x}".\
+            format(ord(a[0]) , ord(a[1]) , ord(a[2]), ord(a[3]), ord(a[4]) , ord(a[5]))
+        return b
+    '''
+    Analisar pacote
+    '''
+    def analisePacote(self, packet):
+        lista = []
+        #parse ethernet header
+        eth_length = 14
+        
+        eth_header = packet[:eth_length]
+        eth = unpack('!6s6sH' , eth_header)
+        eth_protocol = socket.ntohs(eth[2])
+        
+        
+        #Destination MAC,  Source MAC, Protocolo
+        lista.append([self.eth_addr(packet[0:6]),self.eth_addr(packet[6:12]), str(eth_protocol)])
+        
+        return lista
+        pass
+    
+if __name__ == "__main__":
+    SniffImap()    
+    
     
